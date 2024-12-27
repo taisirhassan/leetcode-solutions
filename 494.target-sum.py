ef64c492d8e64f691636ed8bf32bc2b723f96855
taisirhassan <taisirisawesome@gmail.com>
2024-12-27 01:46:42 -0500

    Add solutions for LeetCode problems 1014 (Best Sightseeing Pair) and 494 (Target Sum)

---
+++ 494.target-sum.py
@@ -0,0 +1,38 @@
+#
+# @lc app=leetcode id=494 lang=python3
+#
+# [494] Target Sum
+#
+
+# @lc code=start
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        total_sum = sum(nums)
+        dp = [[0] * (2 * total_sum + 1) for _ in range(len(nums))]
+
+        # Initialize the first row of the DP table
+        dp[0][nums[0] + total_sum] = 1
+        dp[0][-nums[0] + total_sum] += 1
+
+        # Fill the DP table
+        for index in range(1, len(nums)):
+            for sum_val in range(-total_sum, total_sum + 1):
+                if dp[index - 1][sum_val + total_sum] > 0:
+                    dp[index][sum_val + nums[index] + total_sum] += dp[
+                        index - 1
+                    ][sum_val + total_sum]
+                    dp[index][sum_val - nums[index] + total_sum] += dp[
+                        index - 1
+                    ][sum_val + total_sum]
+
+        # Return the result if the target is within the valid range
+        return (
+            0
+            if abs(target) > total_sum
+            else dp[len(nums) - 1][target + total_sum]
+        )
+        
+        
+        
+# @lc code=end
+
